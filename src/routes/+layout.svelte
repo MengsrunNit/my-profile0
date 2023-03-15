ef3d641e9152511b70cf1a1ec10ee22e32da87ea
Mengsrun Nit <nitmengsrun@gmail.com>
2023-03-15 16:38:24 -0400

    flexbox

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,67 +1,102 @@
-
 <div>
-<header id = "header">
-<nav>    
-    <h1>Welcome to My Page</h1>
-    <a id= "home" href="/">Home</a>
-    <a id= "about" href="/about">About</a>
-    <a id ="furman" href="https://www.furman.edu/">Furman Page</a>
-</nav>
-</header>
-
+	<header id="header">
+		<div id="top">
+			<img src="image/FurmanLogo.png" />
+			<h1 id="welcome">
+            <b>
+                Welcome to my Page !!
+            </b>
+        </h1>
+		</div>
+		<div id="navigate-bar">
+			<!-- <img src="image/FurmanLogo.png"/> -->
+			<a id="home" href="/">Home</a>
+			<a id="about" href="/about">About</a>
+			<a id="contact">contact</a>
+			<a id="furman" href="https://www.furman.edu/">Furman Page</a>
+		</div>
+	</header>
 </div>
 
 <div id="logo-box">hello</div>
 
+<slot />
 
 <style>
- #logo-box{
-        border:200px;
-        width:100%;
-        height:20pxpx;
-        background-color:black;
+    #navigate-bar{
+        display:flex; 
+        justify-content: flex-start;
     }
+	img {
+		flex-basic: 200px;
+		height: 80px;
+		width: 200px;
+        margin-right: 100px;
+	}
+	#header {
+		display: flex;
+		flex-direction: column;
+        justify-content: flex-start;
+	}
+	#top {
+		display: flex;
+		flex-direction: row;
+        justify-content:first baseline;
+        margin: 10px 40px 10px 10px;
+	}
+	#logo-box {
+		border: 200px;
+		width: 100%;
+		height: 20px;
+		background-color: black;
+	}
 
-    
-#header{
-    width:1300px;
-    height: 150px;
-    background-color:bisque;
-    border: 20px;
-    padding: 10px 10px;
-    text-align: center;
-    margin-left: auto;
-    margin-right:auto;
-}
-#home,#about,#furman{
-border: none;
-  color: white;
-  padding: 16px 32px;
-  text-align: left;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  margin: 4px 2px;
-  transition-duration: 0.4s;
-  cursor: pointer;
-  width: 250px;
-  margin-left:5px;
-  align:left;
-  
-}
-#home{
-    background-color:red;
-    
-}
-#furman{
-    background-color:purple;
-}
-#about{
-    background-color:blue;
-}
-
-
+	#welcome {
+		padding: 2px 5px;
+		margin-left: 200px;
+        color: brown;
+	}
+	#header {
+		display: flex;
+		flex-direction: column;
+		width: 1300px;
+		height: 150px;
+		background: #82e0aa;
+		border: 20px;
+		padding: 3px 5px;
+		text-align: center;
+		margin-left: auto;
+		margin-right: auto;
+        
+	}
+	#home,
+	#about,
+	#furman,
+	#contact {
+		border: 2px solid black;
+		color: white;
+		padding: 7px 6px;
+		text-align: center;
+		text-decoration: none;
+		display: inline-block;
+		font-size: 16px;
+		margin: 2px 1px;
+		transition-duration: 0.4s;
+		cursor: pointer;
+		width: 90px;
+		height: 30px;
+		align: left;
+	}
+	#home {
+		background-color: red;
+	}
+	#furman {
+		background-color: purple;
+	}
+	#about {
+		background-color: blue;
+	}
+	#contact {
+		background-color: midnightblue;
+	}
 </style>
-
-
-<slot></slot>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,9 +1,34 @@
 <h2>1.Dougnut Framwork of sustainability</h2>
-<img src="image/efe.PNG"/>
+<div id = "devided">
+    <div id ="pic1">
+        <img src="image/efe.PNG"/>
+    </div>
+    <div id ="des">
+        <h1>What on Earth is the Doughnut?…</h1>
+        <p>Humanity’s 21st century challenge is to meet the needs of all within the means of the planet. 
+            In other words, to ensure that no one falls short on life’s essentials 
+            (from food and housing to healthcare and political voice), while ensuring that collectively we do not overshoot our pressure on Earth’s
+             life-supporting systems, on which we fundamentally depend – such as a stable climate, fertile soils, 
+             and a protective ozone layer. The Doughnut of social and planetary boundaries is a playfully serious approach to framing 
+             that challenge, and it acts as a compass for human progress this century.</p>
+    </div>
+    <div id = "pic2">
+        <img src="image/library.jpg"/>
+    </div>
+</div>
+
+
+
 <a id="colorpage" href="/about/Color"> Color </a>
 
 
 <style>
+#devided{
+display: flex; 
+flex-direction: row;
+flex-wrap: nowrap;
+
+}
 img{
     width:600px;
     height:500px;
@@ -28,5 +53,9 @@ h2{
   width: 250px;
   box-sizing: border-box;
   background-color:red;
+}
+#pic1,#pic2, #des{
+    border: 2px solid black;
+
 }
 </style>
