6165570f2ea110d62166f9ae22c64bbca8544d92
Mengsrun Nit <nitmengsrun@gmail.com>
2023-03-15 16:35:05 -0400

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,74 +1,22 @@
-<div>
-    
-    <div id= "locate-box"></div>
-    
+<div class= "flex">
+    <div class="box-1">1</div>
+    <div class="box-2">2</div>
+    <div class="box-3">3</div>
+    <div class="box-4">4</div>
+    <div class="box-5">5</div>
+    <div class="box-6">6</div>
 </div>
-<h2 id="testing"> Class I took last semester: </h2>
-<button id= "csc121" type ="button"> CSC 121 </button><br>
-<button id= "econ" > Economic 120 </button><br>
-<button id = "Math-cal">Math 150</button><br>
-<button id = "FYW">Pdeudoscience </button><br>
-<a id = "web" href="https://www.furman.edu/academics/economics/">button</a>
+
+
 
 <style>
-    h1{color:black;
-        text-align: center;
-        margin:left;
-        }
-    :global(body){
-        
-        background-color:aqua;
+    .flex{
+        display:flex;
+       
+    }
 
-    }
-   
-    #web,button{
-    border: none;
-  color: white;
-  padding: 16px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  margin: 4px 2px;
-  transition-duration: 0.4s;
-  cursor: pointer;
-  width: 250px;
-  box-sizing: border-box;
-    }
-    #csc121{
-        background-color:blue;
-    }
-    #econ{
-        background-color:red;
-    }
-    #FYW{
-        background-color:orangered;
-    }
-    #Math-cal{
-        background-color:brown;
-    }
-    #testing{
-        padding: 5px 10px;
-        font-size:larger;
-        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-        font-style: oblique;
-        background-color: yellowgreen;
-
-    }
-    #web{
-        background-color:blue;
-    }
-    #web:hover{
-        background-color:green;
-    }
-   
-
-
-   
+  
 </style>
-<script>
-    let numbers=[1,2,3,4];
-    function addnumber(){}
-</script>
+
 
 
